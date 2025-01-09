cb4a6515f1a789239899527da44483a0ba0be396
Peder <piddi056@gmail.com>
2025-01-09 18:39:43 +0100

    Task 4. Fixed synchronization

--- Exercise1/shared variable/go/foo.go
+++ Exercise1/shared variable/go/foo.go
@@ -3,40 +3,54 @@
 package main
 
 import (
-	. "fmt"
+	"fmt"
 	"runtime"
-	"time"
 )
 
-var i = 0
-
-func incrementing() {
-	//TODO: increment i 1000000 times
-	for j := 0; j < 1000000; j++ {
-		i++
+func counter(inc_channel, dec_channel, done_channel, return_channel chan int) {
+	i := 0
+	doneflag := 0
+	for {
+		select {
+		case <-inc_channel:
+			i++
+		case <-dec_channel:
+			i--
+		case <-done_channel:
+			doneflag++
+			if doneflag >= 2 {
+				return_channel <- i
+				return
+			}
+		}
 	}
-
 }
 
-func decrementing() {
-	//TODO: decrement i 1000000 times
+func incrementing(inc_channel, done_channel chan int) {
 	for j := 0; j < 1000000; j++ {
-		i--
+		inc_channel <- 1
 	}
+	done_channel <- 1
+}
 
+func decrementing(dec_channel, done_channel chan int) {
+	for j := 0; j < 1000000; j++ {
+		dec_channel <- 1
+	}
+	done_channel <- 1
 }
 
 func main() {
-	// What does GOMAXPROCS do? What happens if you set it to 1?
-	runtime.GOMAXPROCS(2) 
-    
+	runtime.GOMAXPROCS(2)
+	inc_channel := make(chan int)
+	dec_channel := make(chan int)
+	done_channel := make(chan int)
+	return_channel := make(chan int)
 
-	// TODO: Spawn both functions as goroutines
-	go incrementing()
-	go decrementing()
+	go incrementing(inc_channel, done_channel)
+	go decrementing(dec_channel, done_channel)
+	go counter(inc_channel, dec_channel, done_channel, return_channel)
 
-	// We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
-	// We will do it properly with channels soon. For now: Sleep.
-	time.Sleep(500 * time.Millisecond)
-	Println("The magic number is:", i)
+	i := <-return_channel
+	fmt.Println(i)
 }
